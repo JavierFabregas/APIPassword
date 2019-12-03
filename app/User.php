168c2c7400bb0b0a75f2a7f33e95efa8d5718842
Fabregas <javier_fabregas_apps1ma1819@cev.com>
2019-12-03 14:04:40 +0100

    Created models(User-Category-Password) / Created file in middleware / Added Helper/Token

--- app/User.php
+++ app/User.php
@@ -2,29 +2,24 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-    use Notifiable;
+    
+	protected $table = 'users';
+    protected $fillable = ['name','email','password'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function register(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->password = $request->password;
+        $user->email = $request->email;
+        $user->save();
+    }
+    public function categories()
+    {
+        return $this->hasMany('App\Category','user_id');
+    }
 }
